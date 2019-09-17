9c902ca221a38d6f4846c80f38aa9b4f82cda1f8
Williams,Matthew <williama@oclc.org>
2019-09-16 21:17:45 -0400

    pull in tooling

---
+++ refill.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
+SCRIPT_DIR=$(dirname ${SCRIPT_PATH})
+
+cd $SCRIPT_DIR
+
+# [
+#     "index.md",
+#     {
+#         "filename": "slide-title.md",
+#         "attr": {
+#             "data-background": "#ff0000"
+#         }
+#     }
+# ]
+
+
+cat > /tmp/$$ <<EOF
+[
+    {
+      "filename": "index.md",
+      "attr":
+      {
+	"data-autoslide": 5000,
+	"data-background-image": "resources/images/malte-wingen-J6fmT2TosSA-unsplash.jpg"
+      }
+    },
+EOF
+
+i=1
+for file in $(find resources/images/ignite -type f | shuf -n 20)
+do
+
+    echo "    {" >> /tmp/$$
+    echo "        \"filename\": \"${i}.html\"," >> /tmp/$$
+    echo "        \"attr\": {" >> /tmp/$$
+    echo "            \"data-background-image\": \"${file}\"," >> /tmp/$$
+    echo "            \"data-background-size\": \"contain\"" >> /tmp/$$
+    echo "        }" >> /tmp/$$
+    echo -n "   }" >> /tmp/$$
+    i=$((i + 1))
+    if [[ $i -lt 21 ]]; then
+	echo "," >> /tmp/$$
+    fi
+done
+echo "]" >> /tmp/$$
+
+mv /tmp/$$ slides/list.json
---
+++ slides/index.md
@@ -0,0 +1,3 @@
+
+# Ignite Karaoke
+
